36b360d201955e0c0be17473d39152ffd65bff3f
Treefarmer <treefarmer@pocosia.com>
2025-11-22 13:53:28 +0800

    fix stdout flush on c-template

--- programs/tni-mod-template/entry.cpp
+++ programs/tni-mod-template/entry.cpp
@@ -19,12 +19,12 @@ void print_scenario_name(Object& modding_api) {
 
     Node gw(v);
     String scenario_name = gw.get("scenario_name");
-    print("scenario ", scenario_name);
+    printf("scenario %s\n", scenario_name.utf8().c_str());
 }
 
 static Variant on_engine_load(Object modding_api) {
 	// this is called when the engine just loaded this mod.
-	print("Hello from a TNI mod, ", somevar);
+	printf("Hello from a TNI mod, %d\n", somevar);
 	somevar += 1;
 	return Nil;
 }
@@ -40,7 +40,7 @@ static Variant on_player_input(Object modding_api, InputEvent event) {
         InputEventKey* key = reinterpret_cast<InputEventKey*>(&event);
         
         if (key->is_pressed() && key->get_keycode() == 32) {
-            print("SPACEEEE! ", somevar);
+            printf("SPACEEEE! %d\n", somevar);
             somevar += 1;
 
 			print_scenario_name(modding_api);
@@ -57,7 +57,7 @@ static Variant on_game_state_ready(Object modding_api) {
 	// this is called when the game is ready.
     Node gw(modding_api.call("get_game_world"));
     String scenario_name = gw.get("scenario_name");
-    print("scenario ", scenario_name);
+    print("scenario ", scenario_name); // this is godot's print, not c's printf
 	return Nil;
 }
 
@@ -72,6 +72,8 @@ static Variant on_device_spawned(Object modding_api, Node device) {
 }
 
 int main() {
+	setvbuf(stdout, NULL, _IONBF, 0); // this needed for autoflushing writes to stdout
+
 	// implement the following functions if you want to modify the game states
 	ADD_API_FUNCTION(on_engine_load, "", "", "Called when the mod is just loaded");
 	ADD_API_FUNCTION(on_game_state_ready, "", "", "Called when the game is ready");
