498331ea3032db626ed9843d7ed408dec501bcb3
Avril112113 <curtisdude12@gmail.com>
2025-11-24 18:35:18 +0000

    Build release with debug info

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ export CXX="riscv64-linux-gnu-g++-12"
 
 mkdir -p .build
 pushd .build
-cmake -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX .. -DCMAKE_BUILD_TYPE=Release -DSTRIPPED=ON
+cmake -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSTRIPPED=OFF
 make -j
 popd
--- zig.cmd
+++ zig.cmd
@@ -2,6 +2,6 @@
 set HERE=%cd%
 if not exist .zig mkdir .zig
 pushd .zig
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DSTRIPPED=ON -DCMAKE_TOOLCHAIN_FILE=%HERE%\cmake\zig-toolchain.cmake
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSTRIPPED=OFF -DCMAKE_TOOLCHAIN_FILE=%HERE%\cmake\zig-toolchain.cmake
 cmake --build .
 popd
