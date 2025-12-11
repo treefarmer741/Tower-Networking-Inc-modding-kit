74840837b6b372037c130b4635b83a86028c0074
Avril112113 <curtisdude12@gmail.com>
2025-12-11 19:04:00 +0000

    Push gd variant from GDObject and GDCallable + Misc supporting changes

--- programs/luajit/utils.cpp
+++ programs/luajit/utils.cpp
@@ -14,30 +14,38 @@ extern "C" {
 #include "tower.hpp"
 
 
+inline Object check_gdobject(lua_State *L, int pos) {
+    uint64_t* ud = (uint64_t*)luaL_checkudata(L, pos, GDObjectMetaTable);
+    Variant v = get_node<Mod>().instance_from_id_(*ud);
+    if (v.get_type() == Variant::Type::NIL || !v.as_object().is_valid()) {
+        luaL_error(L, "Attempt to index object that is no longer valid.");
+    }
+    return v.as_object();
+}
+inline Object test_gdobject(lua_State *L, int pos) {
+    uint64_t* ud = (uint64_t*)luaL_testudata(L, pos, GDObjectMetaTable);
+    if (ud == NULL)
+        return Object(0);
+    Variant v = get_node<Mod>().instance_from_id_(*ud);
+    return v.as_object();
+}
 int push_gd_object(lua_State *L, Object object) {
-    static const char* MetaTableName = "GodotObject";
-
     if (object.address() == 0) {
         lua_pushnil(L);
         return 1;
     }
 
-    int64_t* ud = (int64_t*)lua_newuserdata(L, sizeof(int64_t));
-    *ud = int64_t(object("get_instance_id"));  // object.get_instance_id() wrongly returns int
+    uint64_t* ud = (uint64_t*)lua_newuserdata(L, sizeof(uint64_t));
+    *ud = uint64_t(object("get_instance_id"));  // object.get_instance_id() wrongly returns int
 
-    if (luaL_newmetatable(L, MetaTableName)) {
+    if (luaL_newmetatable(L, GDObjectMetaTable)) {
         lua_pushstring(L, "__name");  // Was added in Lua 5.3, and not in LuaJIT, but it's nice to have anyway.
-        lua_pushstring(L, MetaTableName);
+        lua_pushstring(L, GDObjectMetaTable);
         lua_settable(L, -3);
 
         lua_pushstring(L, "__index");
         lua_pushcfunction(L, [](lua_State *L) -> int {
-            int64_t* ud = (int64_t*)luaL_checkudata(L, 1, MetaTableName);
-            Variant v = get_node<Mod>().instance_from_id_(*ud);
-            if (v.get_type() == Variant::Type::NIL || !v.as_object().is_valid()) {
-                luaL_error(L, "Attempt to index object that is no longer valid.");
-            }
-            Object obj = v.as_object();
+            Object obj = check_gdobject(L, 1);
             // Variant value = to_gd_variant(L, 2);
             if (lua_isstring(L, 2)) {
                 std::string name = lua_tostring(L, 2);
@@ -45,7 +53,13 @@ int push_gd_object(lua_State *L, Object object) {
                 if (!((Mod)get_node()).is_allowed_property(obj, name)) {
                     luaL_error(L, "Banned property accessed: %s", name.c_str());
                 }
-                return push_gd_variant(L, obj.get(name));
+                // TODO: We could temporarily cache GDCallable userdata during this VM call.
+                // TODO: GDNameCall instead of GDCallable to handle `obj:method()`, which can bypass GDCallable using obj.call/obj.callv (more performant and Lua idiomatic)
+                
+                int c = push_gd_variant(L, obj.get(name));
+                printf("GDObject.__index(%s) = #%d ", name.c_str(), c);
+                print_lua_stack(L);
+                return c;
             }
             lua_pushnil(L);
             return 1;
@@ -54,12 +68,7 @@ int push_gd_object(lua_State *L, Object object) {
 
         lua_pushstring(L, "__tostring");
         lua_pushcfunction(L, [](lua_State *L) -> int {
-            int64_t* ud = (int64_t*)luaL_checkudata(L, 1, MetaTableName);
-            Variant v = get_node<Mod>().instance_from_id_(*ud);
-            if (v.get_type() == Variant::Type::NIL || !v.as_object().is_valid()) {
-                luaL_error(L, "Attempt to index object that is no longer valid.");
-            }
-            Object obj = v.as_object();
+            Object obj = test_gdobject(L, 1);
             if (!obj.is_valid()) {
                 lua_pushfstring(L, "GodotObject: INVALID");
             } else {
@@ -74,35 +83,46 @@ int push_gd_object(lua_State *L, Object object) {
 
     return 1;
 }
-int push_gd_callable(lua_State *L, Callable callable) {
-    static const char* MetaTableName = "GodotCallable";
-    // TODO: Handle temporary variant
 
+inline Callable* check_gdcallable(lua_State *L, int pos) {
+    return (Callable*)luaL_checkudata(L, pos, GDCallableMetaTable);
+}
+inline Callable* test_gdcallable(lua_State *L, int pos) {
+    return (Callable*)luaL_testudata(L, pos, GDCallableMetaTable);
+}
+int push_gd_callable(lua_State *L, Callable callable) {
     // `Callable` just holds an index to the real callable.
     Callable* ud = (Callable*)lua_newuserdata(L, sizeof(Callable));
     *ud = callable;
 
-    if (luaL_newmetatable(L, MetaTableName)) {
+    if (luaL_newmetatable(L, GDCallableMetaTable)) {
         lua_pushstring(L, "__name");  // Was added in Lua 5.3, and not in LuaJIT, but it's nice to have anyway.
-        lua_pushstring(L, MetaTableName);
+        lua_pushstring(L, GDCallableMetaTable);
         lua_settable(L, -3);
 
         lua_pushstring(L, "__call");
         lua_pushcfunction(L, [](lua_State *L) -> int {
-            Callable* ud = (Callable*)luaL_checkudata(L, 1, MetaTableName);
+            // Stack: GDCallable, args...
+            Callable* ud = check_gdcallable(L, 1);
             Variant v = Variant(*ud);
-            int nargs = lua_gettop(L)-1;
+            const int args_pos = 2;  // Position on stack where arguments start.
+            int nargs = lua_gettop(L) - (args_pos-1);
             Variant result;
             switch (nargs) {
                 case 0: result = ud->call(); break;
-                case 1: result = ud->call(to_gd_variant(L, 2)); break;
-                case 2: result = ud->call(to_gd_variant(L, 2), to_gd_variant(L, 3)); break;
-                case 3: result = ud->call(to_gd_variant(L, 2), to_gd_variant(L, 3), to_gd_variant(L, 4)); break;
-                case 4: result = ud->call(to_gd_variant(L, 2), to_gd_variant(L, 3), to_gd_variant(L, 4), to_gd_variant(L, 5)); break;
-                case 5: result = ud->call(to_gd_variant(L, 2), to_gd_variant(L, 3), to_gd_variant(L, 4), to_gd_variant(L, 5), to_gd_variant(L, 6)); break;
-                case 6: result = ud->call(to_gd_variant(L, 2), to_gd_variant(L, 3), to_gd_variant(L, 4), to_gd_variant(L, 5), to_gd_variant(L, 6), to_gd_variant(L, 7)); break;
+                case 1: result = ud->call(to_gd_variant(L, args_pos)); break;
+                case 2: result = ud->call(to_gd_variant(L, args_pos), to_gd_variant(L, args_pos+1)); break;
+                case 3: result = ud->call(to_gd_variant(L, args_pos), to_gd_variant(L, args_pos+1), to_gd_variant(L, args_pos+2)); break;
+                case 4: result = ud->call(to_gd_variant(L, args_pos), to_gd_variant(L, args_pos+1), to_gd_variant(L, args_pos+2), to_gd_variant(L, args_pos+3)); break;
+                case 5: result = ud->call(to_gd_variant(L, args_pos), to_gd_variant(L, args_pos+1), to_gd_variant(L, args_pos+2), to_gd_variant(L, args_pos+3), to_gd_variant(L, args_pos+4)); break;
+                case 6: result = ud->call(to_gd_variant(L, args_pos), to_gd_variant(L, args_pos+1), to_gd_variant(L, args_pos+2), to_gd_variant(L, args_pos+3), to_gd_variant(L, args_pos+4), to_gd_variant(L, args_pos+5)); break;
+                case 7: result = ud->call(to_gd_variant(L, args_pos), to_gd_variant(L, args_pos+1), to_gd_variant(L, args_pos+2), to_gd_variant(L, args_pos+3), to_gd_variant(L, args_pos+4), to_gd_variant(L, args_pos+5), to_gd_variant(L, args_pos+6)); break;
+                case 8: result = ud->call(to_gd_variant(L, args_pos), to_gd_variant(L, args_pos+1), to_gd_variant(L, args_pos+2), to_gd_variant(L, args_pos+3), to_gd_variant(L, args_pos+4), to_gd_variant(L, args_pos+5), to_gd_variant(L, args_pos+6), to_gd_variant(L, args_pos+7)); break;
+                // godot-sandbox only supports up to 8 args.
+                // https://github.com/libriscv/godot-sandbox/blob/1553c9f988f9e174e843143a932f5ba488c9ccdc/src/sandbox_syscalls.cpp#L105
+                // We could get around this by creating a godot array and using `ud->call("callv", args)`
                 default:
-                    return luaL_error(L, "GodotCallable.__call Too many arguments");
+                    return luaL_error(L, "GodotCallable.__call Too many arguments");  // luaL_error never returns.
             }
             return push_gd_variant(L, result);
         });
@@ -110,8 +130,12 @@ int push_gd_callable(lua_State *L, Callable callable) {
 
         lua_pushstring(L, "__tostring");
         lua_pushcfunction(L, [](lua_State *L) -> int {
-            Callable* ud = (Callable*)luaL_checkudata(L, 1, MetaTableName);
-            lua_pushfstring(L, "GodotCallable: %p", ud);  // TODO: Can we get the name and/or signature?
+            Callable* ud = test_gdcallable(L, 1);
+            if (ud == NULL) {
+                lua_pushfstring(L, "GodotCallable: INVALID");
+            } else {
+                lua_pushfstring(L, "GodotCallable: %d @ %p", ud->get_variant_index(), ud);  // TODO: Can we get the name and/or signature?
+            }
             return 1;
         });
         lua_settable(L, -3);
@@ -120,6 +144,7 @@ int push_gd_callable(lua_State *L, Callable callable) {
 
     return 1;
 }
+
 int push_gd_variant(lua_State *L, Variant variant) {
     switch (variant.get_type()) {
         case Variant::Type::NIL:
@@ -154,6 +179,8 @@ int push_gd_variant(lua_State *L, Variant variant) {
 }
 
 Variant to_gd_variant(lua_State *L, int pos) {
+    printf("to_gd_variant(L, %d) ", pos);
+    print_lua_stack(L);
     int ltype = lua_type(L, pos);
     switch (ltype) {
         case LUA_TNIL:
@@ -169,6 +196,15 @@ Variant to_gd_variant(lua_State *L, int pos) {
         // TODO: LUA_TTABLE
         // TODO: LUA_TFUNCTION
         // TODO: LUA_TUSERDATA (for GodotObject)
+        case LUA_TUSERDATA:
+            if (Object obj = test_gdobject(L, pos); obj.is_valid())
+                return obj;
+
+            if (Callable* ud = test_gdcallable(L, pos))
+                return *ud;
+            
+            luaL_error(L, "Can not convert userdata to godot variant.");  // This never returns!
+            return Nil;
         // Omitted LUA_TTHREAD, no use for it.
         default:
             printf("to_gd_variant() Unhandled lua type: %s", lua_typename(L, ltype));
@@ -222,21 +258,29 @@ void print_lua_stack(lua_State *L) {
     for (int i=1; i <= top; i++) {
         printf("%d\t%s\t", i, luaL_typename(L,i));
         switch (lua_type(L, i)) {
-        case LUA_TNUMBER:
-            printf("%g\n", lua_tonumber(L,i));
-            break;
-        case LUA_TSTRING:
-            printf("%s\n", lua_tostring(L,i));
-            break;
-        case LUA_TBOOLEAN:
-            printf("%s\n", (lua_toboolean(L, i) ? "true" : "false"));
-            break;
-        case LUA_TNIL:
-            printf("%s\n", "nil");
-            break;
-        default:
-            printf("%p\n", lua_topointer(L,i));
-            break;
+            case LUA_TNIL:
+                printf("%s\n", "nil");
+                break;
+            case LUA_TBOOLEAN:
+                printf("%s\n", (lua_toboolean(L, i) ? "true" : "false"));
+                break;
+            case LUA_TNUMBER:
+                printf("%g\n", lua_tonumber(L, i));
+                break;
+            case LUA_TSTRING:
+                printf("%s\n", lua_tostring(L, i));
+                break;
+            case LUA_TUSERDATA:
+                printf("%p", lua_topointer(L, i));
+                if (Object obj = test_gdobject(L, i); obj.is_valid())
+                    printf("\tGDObject: %p", (void*)obj.address());
+                if (Callable* ud = test_gdcallable(L, i))
+                    printf("\tGDCallable: %d @ %p", ud->get_variant_index(), ud);
+                printf("\n");
+                break;
+            default:
+                printf("%p\n", lua_topointer(L,i));
+                break;
         }
     }
     fflush(stdout);
--- programs/luajit/utils.hpp
+++ programs/luajit/utils.hpp
@@ -2,12 +2,23 @@ extern "C" {
 #include <lua.h>
 }
 
+#define DEBUG(...) printf(__VA_ARGS__); fflush(stdout)
+
+static const char* GDObjectMetaTable = "GodotObject";
+static const char* GDCallableMetaTable = "GodotCallable";
+
 
 template<typename T>
 T lua_touserdata_t(lua_State *L, int pos, const char* metatable_name);
 
+inline Object check_gdobject(lua_State *L, int pos);
+inline Object test_gdobject(lua_State *L, int pos);
 int push_gd_object(lua_State *L, Object object);
+
+inline Callable* check_gdcallable(lua_State *L, int pos);
+inline Callable* test_gdcallable(lua_State *L, int pos);
 int push_gd_callable(lua_State *L, Callable callable);
+
 int push_gd_variant(lua_State *L, Variant variant);
 
 Variant to_gd_variant(lua_State *L, int pos);
