181d3998165ca0833fcbd49b2deb1177023e3c14
Avril112113 <curtisdude12@gmail.com>
2025-12-21 12:29:47 +0000

    Updated Lua high bw switches example

--- lua/high-bandwidth-switches/entry.lua
+++ lua/high-bandwidth-switches/entry.lua
@@ -1,19 +1,18 @@
-function on_engine_load(modding_api)
-    print("High BW capacity switches!", "") -- output won't flush without newline
-    print("1337") -- this flushes the previous output
-    modding_api.call("sanity")
+function on_engine_load()
+    print("High BW capacity switches!")
+    ModApiV1.sanity()
 end
 
-function on_mod_reload(modding_api)
+function on_mod_reload()
     print("Pressed the reload action key (F11), reloading mod...")
 end
 
-function on_device_spawned(modding_api, device)
-    if device.get("device_hardware_class") == 1 then
-
-        local current_bw = device.get("logic_controller").get("installed_nbw")
+---@param device DeviceUnit
+function on_device_spawned(device)
+    if device.device_hardware_class == 1 then
+        local current_bw = device.logic_controller.installed_nbw
         local new_bw = current_bw * 2
-        device.get("logic_controller").set("installed_nbw", new_bw)
-        print("modified bandwidth of " .. device.get("product_name") .. " from " .. current_bw .. " to " .. new_bw)
+        device.logic_controller.installed_nbw = new_bw
+        print("modified bandwidth of " .. device.product_name .. " from " .. current_bw .. " to " .. new_bw)
     end
 end
