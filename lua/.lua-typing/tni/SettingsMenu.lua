fb6ea4d41b5cbfbe72502f2d5627f22b78ae812a
Avril112113 <curtisdude12@gmail.com>
2025-12-22 01:01:15 +0000

    Lua typing, fixed string enum missing quotes

--- lua/.lua-typing/tni/SettingsMenu.lua
+++ lua/.lua-typing/tni/SettingsMenu.lua
@@ -9,16 +9,16 @@
 local SettingsMenu = {}
 ---@enum SettingsMenu.locale_lang_map
 SettingsMenu.locale_lang_map = {
-	["en"] = English,
-	["ab"] = Pseudotest,
-	["zh_CN"] = 中文,
-	["ru"] = русский,
-	["fr"] = français,
-	["es"] = español,
-	["ja"] = 日本語,
-	["uk"] = українська мова,
-	["pt_BR"] = Português Brasileiro,
-	["de"] = Deutsch,
+	["en"] = "English",
+	["ab"] = "Pseudotest",
+	["zh_CN"] = "中文",
+	["ru"] = "русский",
+	["fr"] = "français",
+	["es"] = "español",
+	["ja"] = "日本語",
+	["uk"] = "українська мова",
+	["pt_BR"] = "Português Brasileiro",
+	["de"] = "Deutsch",
 }
 
 ---@return Array<string>
